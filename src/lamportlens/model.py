69e3d6c4bf83cfd68866ee6f6ab19130207eeb5e
Ryft <10768614+propavingk@users.noreply.github.com>
2017-03-14 15:42:00 -0500

    docs: document typescript

--- src/lamportlens/model.py
+++ src/lamportlens/model.py
@@ -14,3 +14,11 @@ from pathlib import Path
 # Solana's own limit: MAX_ACCOUNT_DATA_LEN, 10 MiB.
 MAX_ACCOUNT_DATA_LEN = 10_485_760
 
+
+class FormatError(ValueError):
+    """Raised when a single record fails validation."""
+
+
+@dataclass(frozen=True)
+class AccountRecord:
+    address: str
