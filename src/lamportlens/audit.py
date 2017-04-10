268078d2f61cf6e2fe48797d8a5d9311bf13648c
Ryft <10768614+propavingk@users.noreply.github.com>
2017-04-10 15:28:00 -0500

    docs: document rent

--- src/lamportlens/audit.py
+++ src/lamportlens/audit.py
@@ -19,3 +19,12 @@ from .rent import (
     AccountAssessment,
     assess,
 )
+
+BANDS: list[tuple[str, int, int]] = [
+    ("0 bytes", 0, 0),
+    ("1-99", 1, 99),
+    ("100-999", 100, 999),
+    ("1k-9.9k", 1_000, 9_999),
+    ("10k-99k", 10_000, 99_999),
+    ("100k-999k", 100_000, 999_999),
+    ("1M and above", 1_000_000, MAX_ACCOUNT_DATA_LEN),
