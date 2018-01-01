5f627ff43ef0458031334f60d4beca8168ade61d
Ryft <10768614+propavingk@users.noreply.github.com>
2018-01-01 11:54:00 -0500

    feat: snapshot

--- src/lamportlens/__init__.py
+++ src/lamportlens/__init__.py
@@ -1,3 +1,4 @@
 """LamportLens: rent-exempt and balance hygiene for Solana account snapshots.
 
 The package is offline and deterministic. It reads a JSONL export of account
+records, validates every line, and reports which accounts hold less than the
