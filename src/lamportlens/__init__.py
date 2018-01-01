dcb4f81e34e52303b1ebb44f6c46437263741913
Ryft <10768614+propavingk@users.noreply.github.com>
2018-01-01 12:19:00 -0500

    fix: cli

--- src/lamportlens/__init__.py
+++ src/lamportlens/__init__.py
@@ -2,3 +2,4 @@
 
 The package is offline and deterministic. It reads a JSONL export of account
 records, validates every line, and reports which accounts hold less than the
+rent-exempt minimum, where storage bonds concentrate, and how lamports are
