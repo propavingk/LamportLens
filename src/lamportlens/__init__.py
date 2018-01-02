477478e0b814e3db10a0ca6c66fc00db367d3dfd
Ryft <10768614+propavingk@users.noreply.github.com>
2018-01-02 13:07:00 -0500

    build: owners

--- src/lamportlens/__init__.py
+++ src/lamportlens/__init__.py
@@ -3,3 +3,5 @@
 The package is offline and deterministic. It reads a JSONL export of account
 records, validates every line, and reports which accounts hold less than the
 rent-exempt minimum, where storage bonds concentrate, and how lamports are
+distributed per owner.
+"""
