f782c79127aaa4b2f9659ce29240dc7845c1c0f4
Ryft <10768614+propavingk@users.noreply.github.com>
2018-01-07 14:19:00 -0500

    fix: correct deficit

--- src/lamportlens/cli.py
+++ src/lamportlens/cli.py
@@ -23,3 +23,15 @@ from .report import Analysis, render_json, render_text
 def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         prog="lamportlens",
+        description="Rent-exempt and balance hygiene for captured Solana account snapshots.",
+    )
+    parser.add_argument("--version", action="version", version=f"lamportlens {__version__}")
+    sub = parser.add_subparsers(dest="command", required=True)
+
+    audit_cmd = sub.add_parser("audit", help="full audit report")
+    audit_cmd.add_argument("input", type=Path, help="account snapshot (JSONL)")
+    audit_cmd.add_argument("--format", choices=("text", "json"), default="text")
+    audit_cmd.add_argument("--limit", type=int, default=10, help="list entries shown per section")
+    audit_cmd.add_argument("--output", type=Path, default=None, help="write the report to a file")
+    audit_cmd.add_argument(
+        "--lamports-per-byte",
