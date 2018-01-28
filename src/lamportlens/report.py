17c733b1c9b60b761ecda2ff51f52a708da44218
Ryft <10768614+propavingk@users.noreply.github.com>
2018-01-28 17:24:00 -0500

    feat: add python

---
+++ src/lamportlens/report.py
@@ -0,0 +1,14 @@
+"""Deterministic text and JSON rendering.
+
+The text report is line oriented so two runs diff cleanly in git. Lists are
+truncated with an explicit `... N more` line, never silently cut. Lamport
+amounts are printed raw, in lamports, because this tool never converts units
+on the reader's behalf.
+"""
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from .audit import AuditReport, statuses_in_order
+from .rent import STATUS_UNDERFUNDED
+
