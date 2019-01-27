0c2992e91047dfef71e4ffbb3a06078f08323b7b
Ryft <10768614+propavingk@users.noreply.github.com>
2019-01-27 10:33:00 -0500

    style: clean up rent

--- tests/test_audit.py
+++ tests/test_audit.py
@@ -14,3 +14,12 @@ SAMPLES = Path(__file__).resolve().parents[1] / "samples"
 class CleanSnapshotTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+        records, errors = parse_file(SAMPLES / "clean-snapshot.jsonl")
+        assert not errors
+        cls.report = audit(records)
+
+    def test_counts(self):
+        self.assertEqual(self.report.record_count, 12)
+        self.assertEqual(self.report.status_counts.get(STATUS_FUNDED), 11)
+        self.assertEqual(self.report.status_counts.get(STATUS_EXCLUDED), 1)
+        self.assertEqual(self.report.findings, 0)
