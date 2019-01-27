96eeee33a811214c9e1809f3b147af298aac3eb6
Ryft <10768614+propavingk@users.noreply.github.com>
2019-01-27 10:21:00 -0500

    test: cover owners

--- tests/test_audit.py
+++ tests/test_audit.py
@@ -6,3 +6,11 @@ from pathlib import Path
 
 from lamportlens.audit import audit
 from lamportlens.model import parse_file
+from lamportlens.rent import STATUS_AT_MINIMUM, STATUS_EXCLUDED, STATUS_FUNDED, STATUS_UNDERFUNDED
+
+SAMPLES = Path(__file__).resolve().parents[1] / "samples"
+
+
+class CleanSnapshotTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
