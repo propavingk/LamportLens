0b2842b02fc5410524da8bdc7cdf47b889255b2e
Ryft <10768614+propavingk@users.noreply.github.com>
2019-02-26 09:34:00 -0500

    style: clean up snapshot

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -15,3 +15,12 @@ SAMPLES = Path(__file__).resolve().parents[1] / "samples"
 def run(args: list[str]) -> tuple[int, str]:
     buffer = io.StringIO()
     with contextlib.redirect_stdout(buffer):
+        code = main(args)
+    return code, buffer.getvalue()
+
+
+class ExitCodeTests(unittest.TestCase):
+    def test_clean_snapshot_exits_zero(self):
+        code, output = run(["audit", str(SAMPLES / "clean-snapshot.jsonl")])
+        self.assertEqual(code, 0)
+        self.assertIn("FINDINGS: 0", output)
