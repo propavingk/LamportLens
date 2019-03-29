b2798eef47e45d67d09c66b41d20aa104f4c3023
Ryft <10768614+propavingk@users.noreply.github.com>
2019-03-29 15:34:00 -0500

    refactor: python

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,3 +7,13 @@ from pathlib import Path
 
 from lamportlens.model import FormatError, parse_record, parse_text
 
+SAMPLES = Path(__file__).resolve().parents[1] / "samples"
+
+
+class ParseRecordTests(unittest.TestCase):
+    def test_minimal_valid_record(self):
+        record = parse_record(
+            {"address": "A", "lamports": 1, "data_len": 0, "owner": "O"}, 1
+        )
+        self.assertEqual(record.data_len, 0)
+        self.assertFalse(record.executable)
