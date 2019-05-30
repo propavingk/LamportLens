4a6ea29660cf62a87d1839a40a86d59c4d9536ed
RozuniFix72 <228983121+RozuniFix72@users.noreply.github.com>
2019-05-30 17:45:00 -0500

    refactor: owners

--- samples/build_fixture.py
+++ samples/build_fixture.py
@@ -11,3 +11,14 @@ rebuilds them byte for byte:
 - samples/broken-lines.jsonl: validation errors only, plus two good records.
 
 Owner program ids that appear are real, well-known Solana program addresses
+(system, SPL token, associated token, token-2022, BPF upgradeable loader).
+Account addresses are synthetic and clearly patterned; no real account is
+represented.
+
+Balances are chosen to sit on the arithmetic boundaries of the current
+lamports-per-byte step (6333): an SPL token account of 165 bytes has a
+minimum of (128 + 165) * 6333 = 1,855,569 lamports, and a zero-byte account
+has a minimum of 128 * 6333 = 810,624 lamports. The historical rate (6960)
+gives the familiar 2,039,280 and 890,880 lamports for the same sizes.
+"""
+from __future__ import annotations
