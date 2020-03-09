27824c7addbd33894f9487c28723d91a14f76119
Ryft <10768614+propavingk@users.noreply.github.com>
2020-03-09 09:25:00 -0500

    docs: document lamports

--- verifier/src/audit.ts
+++ verifier/src/audit.ts
@@ -20,3 +20,15 @@ export const BANDS: Array<[string, number, number]> = [
   ["1-99", 1, 99],
   ["100-999", 100, 999],
   ["1k-9.9k", 1_000, 9_999],
+  ["10k-99k", 10_000, 99_999],
+  ["100k-999k", 100_000, 999_999],
+  ["1M and above", 1_000_000, 10_485_760],
+];
+
+export interface AuditResult {
+  records: number;
+  findings: number;
+  statusCounts: Record<string, number>;
+  totals: { locked: number; balance: number; deficit: number; reclaimable: number };
+  bands: Array<{ label: string; accounts: number; locked: number }>;
+  underfunded: Array<{ address: string; owner: string; lamports: number; minimum: number; deficit: number }>;
