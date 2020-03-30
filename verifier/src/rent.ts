468beb31898f839ab17ab30aac8f51f131d6eb1d
Ryft <10768614+propavingk@users.noreply.github.com>
2020-03-30 14:50:00 -0500

    refactor: cli

---
+++ verifier/src/rent.ts
@@ -0,0 +1,5 @@
+// Independent TypeScript implementation of the lamportlens rent arithmetic.
+//
+// This module exists to cross-check the Python core on the same fixtures. It
+// shares the format contract in docs/FORMAT.md, not code. Standard library
+// only (JSON, process, fs), no runtime dependencies.
