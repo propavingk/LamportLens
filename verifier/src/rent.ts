c1f5ef54aaecd76200781927a53bb59b8519c174
Ryft <10768614+propavingk@users.noreply.github.com>
2020-03-30 15:15:00 -0500

    feat: add report

--- verifier/src/rent.ts
+++ verifier/src/rent.ts
@@ -3,3 +3,9 @@
 // This module exists to cross-check the Python core on the same fixtures. It
 // shares the format contract in docs/FORMAT.md, not code. Standard library
 // only (JSON, process, fs), no runtime dependencies.
+
+export const ACCOUNT_STORAGE_OVERHEAD = 128;
+
+export const RATE_PRESETS: Record<string, number> = {
+  salvage: 696,
+  "simd-0437-5": 696,
