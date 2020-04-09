82d31c51a1c922040697f61b55e04c2d10b759a7
Ryft <10768614+propavingk@users.noreply.github.com>
2020-04-09 17:18:00 -0500

    style: clean up lamports

--- verifier/src/rent.ts
+++ verifier/src/rent.ts
@@ -22,3 +22,9 @@ export const DEFAULT_LAMPORTS_PER_BYTE = 6333;
 export const STATUS_UNDERFUNDED = "underfunded";
 export const STATUS_AT_MINIMUM = "at-minimum";
 export const STATUS_BARELY_ABOVE = "barely-above";
+export const STATUS_FUNDED = "funded";
+export const STATUS_EXCLUDED = "executable-excluded";
+
+export const BARELY_ABOVE_RATIO = 1.01;
+
+export const MAX_ACCOUNT_DATA_LEN = 10_485_760;
