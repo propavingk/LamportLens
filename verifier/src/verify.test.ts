e26feaa6304cff3cf8c2da79e1bada21d0641ed2
Ryft <10768614+propavingk@users.noreply.github.com>
2020-04-15 11:07:00 -0500

    build: typescript

--- verifier/src/verify.test.ts
+++ verifier/src/verify.test.ts
@@ -5,3 +5,10 @@ import { readFileSync } from "node:fs";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 import { test } from "node:test";
+import assert from "node:assert/strict";
+
+import { auditText } from "./audit.js";
+import { AccountRecord, assess, minimumBalance } from "./rent.js";
+
+const here = dirname(fileURLToPath(import.meta.url));
+const samples = join(here, "..", "..", "samples");
