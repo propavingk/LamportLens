4bf6eebf1c67ae127a7c19f227eaa13c16907f5d
Ryft <10768614+propavingk@users.noreply.github.com>
2020-05-18 13:12:00 -0500

    build: bands

--- verifier/src/verify.ts
+++ verifier/src/verify.ts
@@ -6,3 +6,12 @@
 //
 // Prints the audit result as JSON on stdout, so scripts/parity.py can compare
 // it with the Python report. Exit codes: 0 clean, 1 findings, 2 usage or
+// input error.
+
+import { readFileSync } from "node:fs";
+import { auditText } from "./audit.js";
+import { DEFAULT_LAMPORTS_PER_BYTE, RATE_PRESETS } from "./rent.js";
+
+function main(argv: string[]): number {
+  const args = argv.slice(2);
+  let path: string | null = null;
